0d849093d37270836d7e748aa2f5c938bbd3dbef
Yashaswin M <yashaskamble02@gmail.com>
2026-04-30 10:17:52 +0530

    Add Analytics to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <App />
+    <Analytics />
   </BrowserRouter>
 );
